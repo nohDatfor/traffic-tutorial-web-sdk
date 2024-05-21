536b3a594496a42a44fe1d6bb98554a22b9b3476
Nuño <nuno.orgaz@datfor.com>
2024-05-21 10:45:37 +0200

    apiKey y coordenadas orig

--- traffic.js
+++ traffic.js
@@ -1,5 +1,12 @@
-var apiKey = "sG1AXGg2PrKb2ZQPkc16sPLAsSg8lFij";
+
+/*Código Original*/
+var apiKey = "54TfTRUaw3CRZsraCfw2SI8vPrj2VyX6";
 var centerCoords = [4.89218, 52.37187];
+
+/*Código para Aviator
+var apiKey = "sG1AXGg2PrKb2ZQPkc16sPLAsSg8lFij";
+var centerCoords = [-2.416493, 36.888349];
+*/
 var initialZoom = 13;
 var map = tt.map({
     key: apiKey,
